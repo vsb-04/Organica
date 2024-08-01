66aa592623bf1219c42f1bd4c4714afe8deab31d
vsb-04 <139764289+vsb-04@users.noreply.github.com>
2024-08-01 12:22:06 +0530

    Delete src directory

--- src/Component/Protected.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-export const Protected = ({ isSignedIn, children }) => {
-    if (!isSignedIn) {
-        return <Navigate to="/login" replace />
-      }
-      return children
-}
-
