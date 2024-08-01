6dd5ceaa3c87e784254e962ae6fff7033ba492a6
vsb-04 <139764289+vsb-04@users.noreply.github.com>
2024-08-01 12:22:49 +0530

    Add files via upload

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Home } from './Pages/Home';
+import { ProductDetails } from './Pages/ProductDetails';
+import { Router } from './Routes/Router';
+
+function App() {
+  return (
+    <>
+    <Router/>
+   
+    </>
+  );
+}
+
+export default App;
